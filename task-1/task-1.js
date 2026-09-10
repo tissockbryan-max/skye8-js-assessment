59ff50da4824d35dcb1d76290fea68d92c6705aa
Ranjoy <tissockbryan@gmail.com>
2026-09-10 18:06:53 +0100

    Wrote function for generating ids

--- task-1/task-1.js
+++ task-1/task-1.js
@@ -50,6 +50,11 @@ function validateExpense(name, amount) {
   }
 }
 
+// Generates a unique id for each expense.
+function generateId() {
+  return Math.floor(Math.random() * 50) + 1;
+}
+
 // TODO [T1-02]: Add a validated expense to state and re-render.
 function addExpense(name, amount) {
   expenses.push({
